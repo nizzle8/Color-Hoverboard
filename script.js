734a92927020b24d74d4d3362739f1013305cc33
Mathew Edwards <mathew.edwards@hotmail.co.uk>
2022-10-16 08:11:25 +0200

    added pastel colour option

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const container = document.getElementById("container");
-const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
+const defaultBtn = document.getElementById("default");
+const pastelBtn = document.getElementById("pastel");
+let colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
+const pastelColors = ['#ffadad', '#ffd6a5', '#fdffb6', '#caffbf', '#9bf6ff', '#a0c4ff', '#bdb2ff', '#ffc6ff', '#fffffc'];
 const SQUARES = 500;
+
+pastelBtn.addEventListener("click", () => {
+    if(!pastelBtn.classList.contains("active")) {
+        pastelBtn.classList.add("active");
+    }
+});
+
+defaultBtn.addEventListener("click", () => {
+    if(pastelBtn.classList.contains("active")) {
+        pastelBtn.classList.remove("active");
+    }
+});
 
 for(let i = 0; i < SQUARES; i++) {
     const square = document.createElement("div");
@@ -13,9 +28,16 @@ for(let i = 0; i < SQUARES; i++) {
 }
 
 function setColor(element) {
-    const color = getRandomColor();
-    element.style.background = color;
-    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+    let color;
+    if(pastelBtn.classList.contains("active")){
+        color = getPastelColor();
+        element.style.background = color;
+        element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+    } else {
+        color = getRandomColor();
+        element.style.background = color;
+        element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+    }
 }
 
 function removeColor(element) {
@@ -25,4 +47,8 @@ function removeColor(element) {
 
 function getRandomColor() {
     return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function getPastelColor() {
+    return pastelColors[Math.floor(Math.random() * pastelColors.length)];
 }
